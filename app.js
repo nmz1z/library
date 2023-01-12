8edc0ce7672f365eacea95083b57c468e8772447
nmz1z <118689160+nmz1z@users.noreply.github.com>
2023-01-11 21:04:41 -0300

    initial code, not functional

---
+++ app.js
@@ -0,0 +1,68 @@
+// load books array
+
+// draw books in DOM based on array
+
+// ADD:
+    // open modal
+    // save
+
+// EDIT:
+    // open modal w/ edit flah
+    // save
+
+// function setUpLibrary
+
+// function openModal
+
+// function saveBook
+function saveBook(inputs, book){
+
+}
+
+// add book
+function addBook(){
+  const book = {
+    body,
+    title,
+    author,
+    image,
+    pages,
+  }
+
+  return book;
+}
+
+// function toggleRead
+
+// function drawBookInDOM()
+/*
+
+*/
+
+// function loadStorage
+function loadLocalStorage(){
+  return localStorage.getItem("library")
+  ? JSON.parse(localStorage.getItem("library"))
+  : [];
+}
+
+// -- OBJECTS
+
+const modalWindow = {
+  dom,
+  title,
+  author,
+  image,
+  validator,
+  pages,
+}
+// -- EVENTS
+
+
+// -- INIT
+
+// -- VARS
+const booksContainer = document.querySelector('.container');
+
+// loadStorage
+// setUpLibrary
