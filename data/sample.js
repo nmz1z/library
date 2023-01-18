53a458f03e6789dd7210bfb18e7f88c473cd57c8
nmz1z <118689160+nmz1z@users.noreply.github.com>
2023-01-18 17:52:56 -0300

    added

---
+++ data/sample.js
@@ -0,0 +1,41 @@
+const sample = [
+  {
+    data:{
+      title: 'The Lord of the Rings',
+      author: 'J. R. R. Tolkien',
+      image: "https://m.media-amazon.com/images/I/51kfFS5-fnL._AC_SY1000_.jpg" ,
+      pages: 1200,
+    },
+    status: {
+      current: 'In progress',
+      pages: 510,
+    },
+    id: 'S01',
+  },
+  {
+    data:{
+      title: 'Neuromancer',
+      author: 'Willian Gibson',
+      image: "https://m.media-amazon.com/images/I/91Bx5ilP+EL.jpg",
+      pages: 300,
+    },
+    status: {
+      current: 'Plan to read',
+      pages: 50,
+    },
+    id: 'S02',
+  },
+  {
+    data:{
+      title: '1984',
+      author: 'George Orwell',
+      image: "https://i.pinimg.com/564x/43/75/b7/4375b7d9bf24b88aa53744b417227485.jpg",
+      pages: 330,
+    },
+    status: {
+      current: 'Read',
+      pages: 330,
+    },
+    id: 'S03',
+  },
+]
